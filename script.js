b899dd1d6a235e6c6094f22e2ad4691a14e98f5c
Scotty Yackzan <syackzan@gmail.com>
2021-08-11 13:27:55 -0700

    Added Randomize Function to Game

--- script.js
+++ script.js
@@ -4,33 +4,62 @@ var lossCount = 0;
 var tieCount = 0;
 var playerHand;
 var aiHand;
+var value = 3;
 
-function playGame (playerHand) {
+prompt ("Enter R, P, or S", playerHand);
 
-if(playerHand == P) {
-    if(aiHand == S) {
+var array = ["R", "P", "S"]
+
+function getRandomInt (value){
+    return Math.floor(Math.random() * value);
+}
+
+aiHand = getRandomInt (value);
+
+alert(aiHand);
+
+function playGame (playerHand, aiHand) {
+
+/* if (playerHand == aiHand) {
+    //Tie
+    //Add to Tie
+} else if (playerHand < aiHand) {
+
+} */
+
+if(playerHand == "P" || "p") {
+    if(aiHand == "S" || "s") {
         lossCount = lossCount + 1;
-    }else if(aiHand == R) {
+        alert ("You Lost");
+    }else if(aiHand == "R") {
+        winCount = winCount + 1;
+        alert ("You Win")
+    }else {
+        tieCount = tieCount + 1;
+        alert ("You Tie")
+    }
+}
+else  if(playerHand == "S") {
+    if(aiHand == "R") {
+        lossCount = lossCount + 1;
+    }else if(aiHand == "P") {
         winCount = winCount + 1;
     }else {
         tieCount = tieCount + 1;
     }
 }
-else  if(playerHand == S) {
-    if(aiHand == R) {
+else(playerHand == "R") 
+    if(aiHand == "P") {
         lossCount = lossCount + 1;
-    }else if(aiHand == P) {
+    }else if(aiHand == "S") {
         winCount = winCount + 1;
     }else {
         tieCount = tieCount + 1;
     }
 }
-else(playerHand == R) 
-    if(aiHand == P) {
-        lossCount = lossCount + 1;
-    }else if(aiHand == S) {
-        winCount = winCount + 1;
-    }else {
-        tieCount = tieCount + 1;
-    }
+
+while (true){
+
+    playGame(playerHand, aiHand)
+
 }
